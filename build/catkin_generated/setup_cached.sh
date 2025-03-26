fa39dced223e321dec5f1f674036518317ecc99e
ChuyaoZellFu <1365353973@qq.com>
2025-03-26 23:03:36 +0800

    projection_node finished

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,11 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/slam327/syn_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/slam327/syn_ws/devel/lib:$LD_LIBRARY_PATH"
-export PATH='/opt/ros/noetic/bin:/home/slam327/anaconda3/envs/rosenv/bin:/home/slam327/anaconda3/condabin:/home/slam327/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
-export PKG_CONFIG_PATH="/home/slam327/syn_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/slam327/syn_ws/build'
-export PYTHONPATH="/home/slam327/syn_ws/devel/lib/python3/dist-packages:$PYTHONPATH"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/slam327/syn_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/slam327/syn_ws/src:$ROS_PACKAGE_PATH"
